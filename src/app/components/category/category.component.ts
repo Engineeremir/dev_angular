d57764e51231bc9ca482033b722387c2ef77a246
emirhan.kalem320 <emirhan.kalem320@gmail.com>
2021-11-13 01:04:01 +0300

    category operations

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -9,19 +9,30 @@ import { CategoryService } from 'src/app/serivces/category.service';
 })
 export class CategoryComponent implements OnInit {
 
-  categories : Category[]=[];
+  categories: Category[] = [];
+  currentCategory:Category;
 
-  constructor(private categoryService:CategoryService) { }
+  constructor(private categoryService: CategoryService) { }
 
   ngOnInit(): void {
     this.getCategories();
   }
 
-  getCategories(){
-    this.categoryService.getCategories().subscribe(response=>{
+  getCategories() {
+    this.categoryService.getCategories().subscribe(response => {
       this.categories = response.data
 
     })
 
-}
+  }
+  setCurrentCategory(category: Category) {
+    this.currentCategory=category
+  }
+  getCurrentCategoryClass(category:Category){
+    if (category==this.currentCategory) {
+      return "list-group-item active";
+    }else{
+      return "list-group-item"
+    }
+  }
 }
