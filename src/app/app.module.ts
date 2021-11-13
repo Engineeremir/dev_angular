be0a630b5d2da811be40dde1c944375b3e17a1d8
emirhan.kalem320 <emirhan.kalem320@gmail.com>
2021-11-13 17:05:01 +0300

    add product component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,12 @@ import { ProductComponent } from './components/product/product.component';
 import { CategoryComponent } from './components/category/category.component';
 import { NaviComponent } from './components/navi/navi.component';
 import { VatAddedPipe } from './pipes/vat-added.pipe';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { FilterPipePipe } from './pipes/filter-pipe.pipe';
 
 import { ToastrModule } from 'ngx-toastr';
 import { CartSummaryComponent } from './components/cart-summary/cart-summary.component';
+import { ProductAddComponent } from './components/product-add/product-add.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,8 +22,10 @@ import { CartSummaryComponent } from './components/cart-summary/cart-summary.com
     CategoryComponent,
     NaviComponent,
     CartSummaryComponent,
+    ProductAddComponent,
     VatAddedPipe,
-    FilterPipePipe
+    FilterPipePipe,
+
   ],
   imports: [
     BrowserModule,
@@ -30,6 +33,7 @@ import { CartSummaryComponent } from './components/cart-summary/cart-summary.com
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
     ToastrModule.forRoot({
       positionClass:"toast-bottom-right"
 
